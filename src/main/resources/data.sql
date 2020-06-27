76bda110c63aca6467406b8d9119e141cdcdfec0
a <a>
2020-06-27 16:39:02 -0500

    change address language

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO client (id, first_name, last_name,address,identification) VALUES
-  (1, 'Carlos', 'Giraldo','Calle 1 # 2','55454661'),
-  (2, 'Limberg', 'Austris','Cra 8 # 35','45544664');
+  (1, 'Carlos', 'Giraldo','Street 1 # 2','55454661'),
+  (2, 'Limberg', 'Austris','Street 8 # 35','45544664');
