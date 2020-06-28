7fcb7f289276b05316a135a81aa8b18feb95dbcd
a <a>
2020-06-28 17:49:26 -0500

    swagger doc

--- src/main/java/com/hiberus/client/config/ClientConfig.java
+++ src/main/java/com/hiberus/client/config/ClientConfig.java
@@ -1,10 +1,27 @@
 package com.hiberus.client.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 @ComponentScan("com.hiberus.commons.handler")
 public class ClientConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.hiberus.client")).build();
+	}
 
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("client service").description("Service for client CRUD operations").build();
+	}
 }
--- src/main/java/com/hiberus/client/controller/ClientController.java
+++ src/main/java/com/hiberus/client/controller/ClientController.java
@@ -11,6 +11,11 @@ import com.hiberus.client.service.ClientService;
 import com.hiberus.commons.dto.ClientDTO;
 import com.hiberus.commons.dto.JsonDTO;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class ClientController {
 
@@ -18,6 +23,10 @@ public class ClientController {
 	private ClientService clientService;
 
 	@GetMapping("clients/{id}")
+	@ApiOperation(value = "Get a client by id", httpMethod = "GET")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "The client information"),
+			@ApiResponse(code = 400, message = "The client was not found") })
+	@ApiImplicitParam(name = "id", example = "1", dataType = "long")
 	public ResponseEntity<JsonDTO<ClientDTO>> index(@PathVariable Long id) {
 		return new ResponseEntity<>(new JsonDTO<>(clientService.getClient(id)), HttpStatus.OK);
 	}
